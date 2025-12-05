7c7b62945f490768c820ea82f166930171e584a1
Ray-san19 <rayanemoutairou@gmail.com>
2025-12-05 14:05:57 +0100

    add ProjectDevops

---
+++ ProjetDevops.java
@@ -0,0 +1,30 @@
+public class ProjetDevops {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez le premier nombre : ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Entrez le deuxième nombre : ");
+        double b = scanner.nextDouble();
+
+        double somme = a + b;
+        double produit = a * b;
+        double difference = a - b;
+
+        double division;
+        if (b != 0) {
+            division = a / b;
+        } else {
+            division = Double.NaN;
+        }
+
+        System.out.println("\nRésultats :");
+        System.out.println("Somme = " + somme);
+        System.out.println("Produit = " + produit);
+        System.out.println("Différence = " + difference);
+        System.out.println("Division = " + division);
+
+        scanner.close();
+    }
+}
